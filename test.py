dc5ce5d5b9303efe1dba445db5d537844194d911
Seungw0o <tmddn2703@gmail.com>
2023-05-25 00:34:44 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+if __name__ == "__main__":
+
+    x = np.array([0, 1, 2, 3, 4])
+    y = np.array([0, 1, 2, 3, 4])
+
+    print(np.linalg.norm(np.array([-0.31622777 -0.9486833])-np.array([0.31622777 -0.9486833])))
